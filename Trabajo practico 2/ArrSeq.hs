14ea2f30ef232c550e23e2bab2645727d764dbc5
sebamoralesasd <sebasuni96@gmail.com>
2020-06-08 22:20:49 -0300

    ADD: funciones restantes

--- Trabajo practico 2/ArrSeq.hs
+++ Trabajo practico 2/ArrSeq.hs
@@ -65,10 +65,13 @@ showlAS ar
 joinAS :: A.Arr (A.Arr a) -> A.Arr a
 joinAS = A.flatten
 
+
+-- Función de contracción para reduce y para scan en arreglos
+-- de tamaño impar
 contractAS :: (a -> a -> a) -> A.Arr a -> Int -> A.Arr a
 contractAS f ar n = tabulateAS (\i ->
   if i == div n 2
-    then nthAS ar ((lengthAS ar) - 1)
+    then nthAS ar (i*2)
     else f (nthAS ar (i*2)) (nthAS ar (i*2 + 1))) (div (n+1) 2)
 
 reduceAS :: (a -> a -> a) -> a -> A.Arr a -> a
@@ -78,8 +81,52 @@ reduceAS f e ar
   | otherwise = let arCont = contractAS f ar (lengthAS ar)
        in reduceAS f e arCont
 
+-- Función de contracción para scan en arreglos de tamaño par
+contractParAS :: (a -> a -> a) -> A.Arr a -> Int -> A.Arr a
+contractParAS f ar n = tabulateAS (\i -> let a = nthAS ar (2*i)
+                                             b = nthAS ar (2*i + 1)
+                                         in f a b) (div n 2)
+
+-- Función de expansión
+expandAS :: (a -> a -> a) -> A.Arr a -> A.Arr a -> Int -> A.Arr a
+expandAS f arCont arOr n = tabulateAS (\i ->
+  if mod i 2 == 0
+    then nthAS arCont (div i 2)
+    else let a = nthAS arCont (div i 2)
+             b = nthAS arOr (i-1)
+         in f a b) n
+
 scanAS :: (a -> a -> a) -> a -> A.Arr a -> (A.Arr a, a)
-scanAS = undefined
+scanAS f e ar
+  | isEmptyAS ar = (ar, e)
+  | isSingleton ar = let el = firstAS ar
+                     in (singletonAS e, f e el)
+  | otherwise = let nElem = lengthAS ar
+                    arCont = if mod nElem 2 == 0
+                               then contractParAS f ar nElem
+                               else contractAS f ar nElem
+                    (r, t) = scanAS f e arCont
+                in (expandAS f r ar nElem, t)
 
 fromListAS :: [a] -> A.Arr a
 fromListAS = A.fromList
+
+
+-- Instanciación de secuencia en arreglos
+instance Seq A.Arr where
+  emptyS = emptyAS
+  singletonS = singletonAS
+  lengthS = lengthAS
+  nthS = nthAS
+  tabulateS = tabulateAS
+  mapS = mapAS
+  filterS = filterAS
+  appendS = appendAS
+  takeS = takeAS
+  dropS = dropAS
+  showtS = showtAS
+  showlS = showlAS
+  joinS = joinAS
+  reduceS = reduceAS
+  scanS = scanAS
+  fromList = fromListAS
