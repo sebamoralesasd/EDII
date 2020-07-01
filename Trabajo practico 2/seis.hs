764ac9ae7118f4bc44b8ba6b07d66ba6a3149da2
sebamoralesasd <sebasuni96@gmail.com>
2020-07-01 14:42:54 -0300

    ADD: ejercicios 3 y 4a

--- Trabajo practico 2/seis.hs
+++ Trabajo practico 2/seis.hs
@@ -2,6 +2,23 @@ module Seis where
 
 import Seq
 import ArrSeq
+import Par
+
+-- Funciones útiles
+isEmptyS :: (Seq s) => s a -> Bool
+isEmptyS ar = lengthS ar == 0
+
+isSingleton :: (Seq s) => s a -> Bool
+isSingleton ar = lengthS ar == 1
+
+firstS :: (Seq s) => s a -> a
+firstS ar = nthS ar 0
+
+maxSeq :: (Seq s) => s Int -> s Int
+maxSeq ar = fst $ scanS (max) 0 ar
+
+dyc :: (Seq s) => s a -> (b -> b -> b) -> b -> (a -> b) -> b
+dyc ar combine val base = reduceS combine val (mapS base ar)
 
 -- Ej 1
 divF :: Int -> Int -> Float
@@ -52,3 +69,39 @@ fibSeq :: (Seq s) => Int -> s Int
 fibSeq n = let ar = tabulateS (\_ -> fibMat) n
                matSeq = fst $ scanS (multMat) fibMat ar
            in mapS (getFib) matSeq
+
+-- Ej 4
+data Paren = Open | Close
+type Tupla = (Int, Int)
+
+parenVal :: Tupla
+parenVal = (0,0)
+
+parenBase :: Paren -> Tupla
+parenBase Open = (0,1)
+parenBase Close = (1,0)
+
+parenCombine :: Tupla -> Tupla -> Tupla
+parenCombine (cI,aI) (cD,aD) = (max 0 (cD-aI+cI), max 0 (aI-cD+aD))
+
+matchP :: (Seq s) => s Paren -> Tupla
+matchP s = dyc s parenCombine parenVal parenBase
+
+
+-- Ej 3
+reverseSeq :: (Seq s) => s a -> s a
+reverseSeq ar = let n = lengthS ar
+                in tabulateS (\i -> nthS ar (n-1-i)) n
+
+maxAgua :: (Seq s) => s Int -> (s Int, s Int)
+maxAgua hist = maxSeq hist ||| reverseSeq (maxSeq (reverseSeq hist))
+
+difAgua :: (Seq s) => s Int -> s Int -> s Int -> s Int
+difAgua hist maxL maxR = tabulateS (dif) (lengthS hist)
+  where dif i = max 0 $ min (nthS maxL i) (nthS maxR i) - nthS hist i
+
+aguaHist :: (Seq s) => s Int -> Int
+aguaHist hist =
+  let (maxL, maxR) = maxAgua hist
+      agua = difAgua hist maxL maxR
+  in reduceS (+) 0 agua
